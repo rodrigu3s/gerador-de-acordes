3096c4628f785210248c434f144b89c39868b19e
rodrigu3s <joaorodrigues9631@gmail.com>
2023-03-11 03:40:26 -0300

    layout para monitor ultrawide

---
+++ src/app/pages/gerador/gerador-de-acordes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-gerador-de-acordes',
+  templateUrl: './gerador-de-acordes.component.html',
+  styleUrls: ['./gerador-de-acordes.component.scss']
+})
+
+export class GeradorDeAcordesComponent {
+
+}
